ab1cdb069ea48958244bb6aabffd347444cd1b32
aastikdas <aastikdas126@gmail.com>
2026-05-31 12:13:29 +0530

    chore: initialize Next.js project with Tailwind and shadcn/ui

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Code Canvas- A playground for your code",
+  description: "Code Canvas is a web-based code editor that allows you to write, run, and share code snippets in various programming languages. It provides a simple and intuitive interface for coding, making it easy for developers of all levels to experiment and collaborate on code projects.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
+    </html>
+  );
+}
