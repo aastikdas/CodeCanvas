de1676e021774cce031835a42cf0353d92eeadb3
aastikdas <aastikdas126@gmail.com>
2026-06-01 14:14:04 +0530

    feat: configure auth flow with Prisma adapter

---
+++ auth.ts
@@ -0,0 +1,73 @@
+import NextAuth from "next-auth"
+import {PrismaAdapter} from "@auth/prisma-adapter"
+import {db} from "@/lib/db"
+import authConfig from "./auth.config"
+ 
+export const { handlers, signIn, signOut, auth } = NextAuth({
+    callbacks:{
+        async signIn({user, account, profile}){
+            if(!user || !account ) return false
+                
+            const existingUser = await db.user.findUnique({
+                where:{
+                    email: user.email!}
+            });
+            if(!existingUser){
+                const newUser = await db.user.create({
+                    data:{
+                        email: user.email!,
+                        name: user.name,
+                        image: user.image,
+                        accounts:{
+                            create:{
+                                type: account.type,
+                                provider: account.provider,
+                                providerAccountId: account.providerAccountId,   
+                                access_token: account.access_token,
+                                refresh_token: account.refresh_token,
+                                expires_at: account.expires_at,
+                                token_type: account.token_type,
+                                scope: account.scope,
+                                id_token: account.id_token,
+                                sessionState: account.session_state
+                            },
+                        },
+                    },
+                });
+                if(!newUser) return false
+            }
+            else{
+                const existingAccount = await db.account.findUnique({
+                    where:{
+                        provider_providerAccountId:{
+                            provider: account.provider,
+                            providerAccountId: account.providerAccountId,
+                        }
+                    }
+                })
+                if (!existingAccount) {
+                    await db.account.create({
+                    data: {
+                        userId: existingUser.id,
+                        type: account.type,
+                        provider: account.provider,
+                        providerAccountId: account.providerAccountId,
+                        refreshToken: account.refresh_token,
+                        accessToken: account.access_token,
+                        expiresAt: account.expires_at,
+                        tokenType: account.token_type,
+                        scope: account.scope,
+                        idToken: account.id_token,
+                        sessionState: account.session_state,
+                    },
+                });
+            }
+            }
+            return true;
+        }
+    },
+    secret: process.env.AUTH_SECRET,
+    adapter: PrismaAdapter(db),
+    session:{strategy:"jwt"},
+    ...authConfig,
+})
