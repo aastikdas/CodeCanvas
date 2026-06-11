baf192cc61b1baf5531d90536e1bcb13914cf127
aastikdas <aastikdas126@gmail.com>
2026-06-11 21:42:11 +0530

    resolved extra import issue

--- features/dashboard/components/project-table.tsx
+++ features/dashboard/components/project-table.tsx
@@ -215,13 +215,13 @@ export default function ProjectTable({
                 <TableCell>
                   <div className="flex items-center gap-2">
                     <div className="w-8 h-8 rounded-full overflow-hidden">
-                      <Image
+                      {/* <Image
                         src={project.user.image || "/placeholder.svg"}
                         alt={project.user.name}
                         width={32}
                         height={32}
                         className="object-cover"
-                      />
+                      /> */}
                     </div>
                     <span className="text-sm">{project.user.name}</span>
                   </div>
