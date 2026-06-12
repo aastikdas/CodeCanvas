bc590019648e7f53f784a617e184932cbb342867
aastikdas <aastikdas126@gmail.com>
2026-06-12 09:25:27 +0530

    added starter-files and playgrounds

---
+++ app/playground/[id]/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+import { Separator } from "@/components/ui/separator";
+import { SidebarInset, SidebarTrigger } from "@/components/ui/sidebar";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { useParams } from "next/navigation"
+
+
+const Page = () => {
+    const {id} = useParams<{id:string}>();
+  return (
+    <TooltipProvider>
+      <>
+        {/* {Todo impllement tree} */}
+        <SidebarInset>
+          <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
+            <SidebarTrigger className="-ml-1" />
+            <Separator className="mr-2 h-4" orientation="vertical" />
+            <div>
+              <div>
+                hi
+              </div>
+            </div>
+          </header>
+        </SidebarInset>
+      </>
+    </TooltipProvider>
+  )
+}
+
+export default Page
