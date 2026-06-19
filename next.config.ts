0aeb66ec0289196d5e9c89b757bb90b133c5bdf5
aastikdas <aastikdas126@gmail.com>
2026-06-19 11:46:59 +0530

    added cors

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,33 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images:{
+    remotePatterns:[
+      {
+        protocol:"https",
+        hostname:'*',
+        port:'',
+        pathname:'/**'
+      }
+    ]
+  },
+  async headers() {
+    return [
+      {
+          source: '/:path*',
+          headers:[
+            {
+              key: 'Cross-Origin-Opener-Policy',
+              value: 'same-origin'
+            },
+            {
+              key: 'Cross-Origin-Embedder-Policy',
+              value: 'require-corp'
+            }
+          ]
+      },
+    ];
+  }
 };
 
 export default nextConfig;
