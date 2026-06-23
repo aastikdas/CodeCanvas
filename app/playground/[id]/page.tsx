46084282ea972a5d3ab827da935681ea82b0edd6
aastikdas <aastikdas126@gmail.com>
2026-06-23 13:33:25 +0530

    -f: created wrappers

--- app/playground/[id]/page.tsx
+++ app/playground/[id]/page.tsx
@@ -45,6 +45,7 @@ import { useWebContainer } from "@/features/webContainers/hooks/useWebContainer"
 import WebContainerPreview from "@/features/webContainers/components/WebContainerPreview";
 import { Spinner } from "@/components/ui/spinner";
 import { findFilePath } from "@/features/playground/lib";
+import { TemplateFolder } from "@/features/playground/types";
 const Page = () => {
     const {id} = useParams<{id:string}>();
     const [isPreviewVisible, setIsPreviewVisible] = useState(true);
@@ -86,6 +87,70 @@ const Page = () => {
       setTemplateData(templateData);
     }
   }, [templateData, setTemplateData, openFiles.length]);
+
+   const wrappedHandleAddFile = useCallback(
+    (newFile: TemplateFile, parentPath: string) => {
+      return handleAddFile(
+        newFile,
+        parentPath,
+        writeFileSync!,
+        instance,
+        saveTemplateData
+      );
+    },
+    [handleAddFile, writeFileSync, instance, saveTemplateData]
+  );
+
+  const wrappedHandleAddFolder = useCallback(
+    (newFolder: TemplateFolder, parentPath: string) => {
+      return handleAddFolder(newFolder, parentPath, instance, saveTemplateData);
+    },
+    [handleAddFolder, instance, saveTemplateData]
+  );
+
+  const wrappedHandleDeleteFile = useCallback(
+    (file: TemplateFile, parentPath: string) => {
+      return handleDeleteFile(file, parentPath, saveTemplateData);
+    },
+    [handleDeleteFile, saveTemplateData]
+  );
+
+  const wrappedHandleDeleteFolder = useCallback(
+    (folder: TemplateFolder, parentPath: string) => {
+      return handleDeleteFolder(folder, parentPath, saveTemplateData);
+    },
+    [handleDeleteFolder, saveTemplateData]
+  );
+
+  const wrappedHandleRenameFile = useCallback(
+    (
+      file: TemplateFile,
+      newFilename: string,
+      newExtension: string,
+      parentPath: string
+    ) => {
+      return handleRenameFile(
+        file,
+        newFilename,
+        newExtension,
+        parentPath,
+        saveTemplateData
+      );
+    },
+    [handleRenameFile, saveTemplateData]
+  );
+
+  const wrappedHandleRenameFolder = useCallback(
+    (folder: TemplateFolder, newFolderName: string, parentPath: string) => {
+      return handleRenameFolder(
+        folder,
+        newFolderName,
+        parentPath,
+        saveTemplateData
+      );
+    },
+    [handleRenameFolder, saveTemplateData]
+  );
 
   const activeFile = openFiles.find((file) => file.id === activeFileId);
   const hasUnsavedChanges = openFiles.some((file) => file.hasUnsavedChanges);
@@ -269,7 +334,15 @@ const Page = () => {
     <TooltipProvider>
       <>
         <TemplateFileTree data={templateData} onFileSelect={handleFileSelect}
-                selectedFile={activeFile}/>
+                selectedFile={activeFile}
+                title="File Explorer"
+                onAddFile={wrappedHandleAddFile}
+                onAddFolder={wrappedHandleAddFolder}
+                onDeleteFile={wrappedHandleDeleteFile}
+                onDeleteFolder={wrappedHandleDeleteFolder}
+                onRenameFile={wrappedHandleRenameFile}
+                onRenameFolder={wrappedHandleRenameFolder}
+                />
         <SidebarInset>
           <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
             <SidebarTrigger className="-ml-1" />
